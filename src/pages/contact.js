7d45be6b294e2e2a8b73f759357082c7bb52e3fa
matthoare <23655704@edgehill.ac.uk>
2020-05-15 15:51:09 +0100

    Styled contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Layout from "../components/layout"
-import Icons from "../components/social-media"
 import Phone from "../images/phone.svg"
 import Email from "../images/email.svg"
 
@@ -15,22 +14,18 @@ const Contact = () => {
         <input id="email" type="email"/>
         <label htmlFor="message">Message</label>
         <textarea name="message" id="message" cols="30" rows="10"></textarea>
-        <button type="submit">Send</button>
+        <button type="submit" className="submit">Send</button>
       </form>
-      <article>
+      <article className="contact-info">
         <section>
-          <Phone/>
+          <Phone className="contact-icon"/>
           <p>Call Me</p>
           <a href="tel:1-847-555-5555">07887770657</a>
         </section>
         <section>
-          <Email/>
+          <Email className="contact-icon"/>
           <p>Email Me</p>
           <a href="mailto:matthoare5@hotmail.co.uk?subject=Portfolio">matthoare5@hotmail.co.uk</a>
-        </section>
-        <section>
-          <p>Follow Me</p>
-          <Icons/>
         </section>
       </article>
     </Layout>
